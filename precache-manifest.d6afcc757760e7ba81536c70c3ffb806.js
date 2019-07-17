8870c1daecbd60b1e8984ed116d1da0570021a65
okabamac <markokaba99@gmail.com>
2019-07-17 11:44:48 +0100

    Updates

---
+++ precache-manifest.d6afcc757760e7ba81536c70c3ffb806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc78dc11783da5e2c19d4b57e657ddb",
+    "url": "/nemsa-react/public/index.html/index.html"
+  },
+  {
+    "revision": "aff9a3ea605b540faee0",
+    "url": "/nemsa-react/public/index.html/static/css/main.a75bca6e.chunk.css"
+  },
+  {
+    "revision": "027a1d834e6c5781ab6d",
+    "url": "/nemsa-react/public/index.html/static/js/2.2843e760.chunk.js"
+  },
+  {
+    "revision": "aff9a3ea605b540faee0",
+    "url": "/nemsa-react/public/index.html/static/js/main.b6fe9129.chunk.js"
+  },
+  {
+    "revision": "b1e5d26a0f8bfc034850",
+    "url": "/nemsa-react/public/index.html/static/js/runtime~main.c304d2ca.js"
+  },
+  {
+    "revision": "038dbd32d31ac61f875c7a8abf453e6c",
+    "url": "/nemsa-react/public/index.html/static/media/girl.038dbd32.svg"
+  },
+  {
+    "revision": "049e3c1c65b3b440fc6e51a8150c17af",
+    "url": "/nemsa-react/public/index.html/static/media/nemsa_logo1.049e3c1c.png"
+  }
+]);
