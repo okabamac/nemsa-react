dead06a1f491b18bd95be9703c82753e98727828
okabamac <markokaba99@gmail.com>
2019-07-17 12:26:28 +0100

    Add a basename for BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-render (
-    <BrowserRouter basename='/build'>
+render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-    </BrowserRouter>, document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
